6390ff037552d378147410592eb3b07eaa3d0300
noname <davidgarciasolis@gmail.com>
2026-04-25 12:50:17 +0200

    Continuacion del ex0

--- ex0/data_processor.py
+++ ex0/data_processor.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 import abc
-from typing import Any
+import typing
 
 class DataProcessor(abc.ABC):
     rank: int
@@ -11,11 +11,11 @@ class DataProcessor(abc.ABC):
         self.rank = -1
 
     @abc.abstractmethod
-    def validate(self, data: Any) -> bool:
+    def validate(self, data: typing.Any) -> bool:
         pass
     
     @abc.abstractmethod
-    def ingest(self, data: Any) -> None:
+    def ingest(self, data: typing.Any) -> None:
         pass
 
     def output(self) -> tuple[int, str]:
@@ -26,18 +26,20 @@ class DataProcessor(abc.ABC):
 
 
 class NumericProcessor(DataProcessor):
-
     def __init__(self):
         super().__init__()
 
-    def validate(self, data: Any) -> bool:
+    def validate(self, data: typing.Any) -> bool:
         if isinstance(data, (int, float)):
-            return True
+            return (True)
         if isinstance(data, list):
-            return all(isinstance(nodo, (int, float)) for nodo in data)
-        return False
+            for nodo in data:
+                if not isinstance(nodo, (int, float)):
+                    return (False)
+            return (True)
+        return (False)
     
-    def ingest(self, data: Any) -> None:
+    def ingest(self, data: typing.Any) -> None:
         if self.validate(data):
             if isinstance(data, list):
                 for nodo in data:
@@ -46,11 +48,32 @@ class NumericProcessor(DataProcessor):
                 self.list_data.append(str(data))
         else:
             raise Exception("Improper numeric data")
+
+
+class TextProcessor(DataProcessor):
+    def __init__(self):
+        super().__init__()
+
+    def validate(self, data: typing.Any) -> bool:
+        if isinstance(data, str):
+            return (True)
+        if isinstance(data, list):
+            for nodo in data:
+                if not isinstance(nodo, str):
+                    return (False)
+            return (True)
+        return (False)
+
+    def ingest(self, data: typing.Any) -> None:
+        pass
 		
 
 
 def main():
     numeric = NumericProcessor()
+    list_numeric = [1,2,3,4,5]
+    text = TextProcessor()
+    list_text = ["Hello", "Nexus", "World"]
 
 
     print("=== Code Nexus - Data Processor ===")
@@ -63,13 +86,25 @@ def main():
         numeric.ingest("foo")
     except Exception as e:
         print(f"Got exception: {e}")
-    for i in range(1, 6):
-        numeric.ingest(i)
-    print(f"Processing data: {numeric.list_data}")
+    print(f"Processing data: {list_numeric}")
+    numeric.ingest(list_numeric)
     print(" Extracting 3 values...")
     for i in range(3):
         response = numeric.output()
-        print(f"Numeric value {response[0]}:")
+        print(f"Numeric value {response[0]}: {response[1]}")
+    print()
+    print("Testing Text Processor...")
+    print(f" Trying to validate input'42': {text.validate(42)}")
+    print(f" Processing data: {list_text}")
+    text.ingest(list_text)
+    print(" Extracting 1 value...")
+    for i in range(1):
+        response = text.output()
+        print(f" Text value {response[0]}: {response[1]}")
+    print()
+    print("Testing Log Processor...")
+    
+
 	
 
 
