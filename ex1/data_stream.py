c678e1a05909cdb97f0541250863fd9c274e9e19
David Garcia Solis <davgarc4@c3r8s3.42madrid.com>
2026-04-25 20:37:40 +0200

    Subida del ex1

--- ex1/data_stream.py
+++ ex1/data_stream.py
@@ -7,7 +7,7 @@ class DataProcessor(abc.ABC):
     rank: int
     list_data: list[str]
 
-    def __init__(self):
+    def __init__(self) -> None:
         self.list_data = []
         self.rank = -1
 
@@ -27,7 +27,7 @@ class DataProcessor(abc.ABC):
 
 
 class NumericProcessor(DataProcessor):
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__()
 
     def validate(self, data: typing.Any) -> bool:
@@ -40,7 +40,8 @@ class NumericProcessor(DataProcessor):
             return (True)
         return (False)
 
-    def ingest(self, data: typing.Any) -> None:
+    def ingest(self, data: int | float | list[int]
+               | list[float] | list[int | float]) -> None:
         if self.validate(data):
             if isinstance(data, list):
                 for nodo in data:
@@ -52,7 +53,7 @@ class NumericProcessor(DataProcessor):
 
 
 class TextProcessor(DataProcessor):
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__()
 
     def validate(self, data: typing.Any) -> bool:
@@ -65,7 +66,7 @@ class TextProcessor(DataProcessor):
             return (True)
         return (False)
 
-    def ingest(self, data: typing.Any) -> None:
+    def ingest(self, data: str | list[str]) -> None:
         if self.validate(data):
             if isinstance(data, list):
                 for nodo in data:
@@ -77,7 +78,7 @@ class TextProcessor(DataProcessor):
 
 
 class LogProcessor(DataProcessor):
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__()
 
     def validate(self, data: typing.Any) -> bool:
@@ -90,7 +91,7 @@ class LogProcessor(DataProcessor):
             return (True)
         return (False)
 
-    def ingest(self, data: typing.Any) -> None:
+    def ingest(self, data: dict[str, str] | list[dict[str, str]]) -> None:
         if self.validate(data):
             try:
                 if isinstance(data, list):
@@ -109,49 +110,92 @@ class LogProcessor(DataProcessor):
 class DataStream():
     list_proc: list[DataProcessor]
 
-    def __init__(self):
+    def __init__(self) -> None:
         self.list_proc = []
-
 
     def register_processor(self, proc: DataProcessor) -> None:
         if isinstance(proc, DataProcessor):
             self.list_proc.append(proc)
         else:
             print("Improper processor data ")
-    
+
     def process_stream(self, stream: list[typing.Any]) -> None:
         for nodo in stream:
-            for proc in list_proc:
+            fail_proc = True
+            for proc in self.list_proc:
                 if proc.validate(nodo):
-                    
+                    proc.ingest(nodo)
+                    fail_proc = False
+                    break
+            if fail_proc:
+                print(f"DataStream error - Can't process element in stream: "
+                      f"{nodo}")
+
+    def print_processors_stats(self) -> None:
+        i: int
+
+        print("== DataStream statistics ==")
+        if self.list_proc:
+            for proc in self.list_proc:
+                i = 0
+                for data in proc.list_data:
+                    i += 1
+                name_proces = f"{proc.__class__.__name__}"
+                name_proces = name_proces.replace("Processor", " Processor")
+                print(f"{name_proces}: total {i + proc.rank + 1}"
+                      f" items processed, remaining {i} on processor")
+        else:
+            print("No processor found, no data")
 
 
-def main():
+def main() -> None:
     numeric = NumericProcessor()
     text = TextProcessor()
     log = LogProcessor()
     list_numeric = [3.14, -1, 2.71]
     uni_numeric = 42
     uni_text = ["Hello world"]
-    list_text = ['Hi','five']
+    list_text = ['Hi', 'five']
     list_log = [
-        {'log_level': 'WARNING', 'log_message': 'Telnet access! Use ssh instead'},
+        {'log_level': 'WARNING',
+         'log_message': 'Telnet access! Use ssh instead'},
         {'log_level': 'INFO', 'log_message': 'User wil is connected'}
     ]
-    data = uni_text, list_numeric, list_log, uni_numeric, list_text
-
+    list_data = uni_text, list_numeric, list_log, uni_numeric, list_text
+    data: list[typing.Any] = []
+    for nodo in list_data:
+        data.append(nodo)
     print("=== Code Nexus - Data Stream ===")
     print()
     print("Initialize Data Stream...")
     stream = DataStream()
+    stream.print_processors_stats()
     print()
-    print("Registering Numeric Processor")
+    name_proces = f"{numeric.__class__.__name__}"
+    name_proces = name_proces.replace("Processor", " Processor")
+    print(f"Registering {name_proces}")
     stream.register_processor(numeric)
     print()
     print(f"Send first batch of data on stream: {data}")
-
-
-
+    stream.process_stream(data)
+    stream.print_processors_stats()
+    print()
+    print("Registering other data processors")
+    stream.register_processor(text)
+    stream.register_processor(log)
+    print("Send the same batch again")
+    stream.process_stream(data)
+    stream.print_processors_stats()
+    print()
+    print("Consume some elements from the data processors: "
+          "Numeric 3, Text 2, Log 1")
+    stream.list_proc[0].output()
+    stream.list_proc[0].output()
+    stream.list_proc[0].output()
+    stream.list_proc[1].output()
+    stream.list_proc[1].output()
+    stream.list_proc[2].output()
+    stream.print_processors_stats()
 
 
 if __name__ == "__main__":
